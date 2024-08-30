68b621c2d79046f2e9980110c44e2a23d489ea4c
yuzen622 <125558430+yuzen622@users.noreply.github.com>
2024-08-30 15:04:43 +0800

    update upload.html

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
-let url = "https://yuzen.serveirc.com/data share";
-//const url = "http://127.0.0.1/data share";
+//let url = "https://yuzen.serveirc.com/data share";
+const url = "http://127.0.0.1/data share";
