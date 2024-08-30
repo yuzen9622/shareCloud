73ef761516c3d295ce405f0982c9ed1c70f1281f
yuzen622 <125558430+yuzen622@users.noreply.github.com>
2024-08-30 23:44:37 +0800

    fix url

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-//let url = "https://yuzen.serveirc.com/data share";
+let url = "https://yuzen.serveirc.com/data share";
 //const url = "http://127.0.0.1/data share";
-const url = "http://35.75.66.92/data share";
+//const url = "http://35.75.66.92/data share";
